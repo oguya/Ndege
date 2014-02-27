c4b5a7eb933c771cb5561c87f45bc55055b9f2b4
oguya <oguyajames@gmail.com>
2014-02-27 10:23:48 +0300

    adding models & working on db

---
+++ ndege/src/main/java/com/droid/ndege/model/BirdImage.java
@@ -0,0 +1,58 @@
+package com.droid.ndege.model;
+
+/**
+ * Created by james on 27/02/14.
+ */
+public class BirdImage {
+
+    public static final String IMAGE_ID = "imageID";
+    public static final String TAG_ID = "tagID";
+    public static final String IMAGE_URL = "imageURL";
+    public static final String SITE_URL = "siteURL";
+
+    private int imageID;
+    private int tagID;
+    private String imageURL;
+    private String siteURL;
+
+    public BirdImage(){}
+
+    public BirdImage(int imageID, int tagID, String imageURL, String siteURL){
+        this.imageID = imageID;
+        this.tagID = tagID;
+        this.imageURL = imageURL;
+        this.siteURL = siteURL;
+    }
+
+    public int getImageID(){
+        return this.imageID;
+    }
+
+    public void setImageID(int imageID){
+        this.imageID = imageID;
+    }
+
+    public int getTagID(){
+        return this.tagID;
+    }
+
+    public void setTagID(int tagID){
+        this.tagID = tagID;
+    }
+
+    public String getImageURL(){
+        return this.imageURL;
+    }
+
+    public void setImageURL(String imageURL){
+        this.imageURL = imageURL;
+    }
+
+    public String getSiteURL(){
+        return this.siteURL;
+    }
+
+    public void setSiteURL(String siteURL){
+        this.siteURL = siteURL;
+    }
+}
---
+++ ndege/src/main/java/com/droid/ndege/model/Tag.java
@@ -0,0 +1,159 @@
+package com.droid.ndege.model;
+
+/**
+ * Created by james on 27/02/14.
+ */
+public class Tag {
+
+    public static final String TAG_ID = "tagID";
+    public static final String BIRD_ID  = "birdID";
+    public static final String ENGLISH_NAME = "englishName";
+    public static final String GENERIC_NAME = "genericName";
+    public static final String SPECIFIC_NAME = "specificName";
+    public static final String RECORDER = "Recorder";
+    public static final String LOCATION = "Location";
+    public static final String COUNTRY = "Country";
+    public static final String LAT = "lat";
+    public static final String LNG = "lng";
+    public static final String XENOCANTOURL = "xenoCantoURL";
+    public static final String SOUND_TYPE = "soundType";
+    public static final String SOUND_URL = "soundURL";
+
+
+    private int tagID;
+    private int birdID;
+    private String englishName;
+    private String genericName;
+    private String specificName;
+    private String recorder;
+    private String location;
+    private String country;
+    private String lat;
+    private String lng;
+    private String xenoCantoURL;
+    private String soundType;
+    private String soundURL;
+
+    public Tag(){}
+
+    public Tag(int tagID, int birdID, String englishName, String genericName,
+               String specificName, String recorder, String location, String country,
+               String lat, String lng, String xenoCantoURL, String soundType, String soundURL){
+
+        this.tagID = tagID;
+        this.birdID = birdID;
+        this.englishName = englishName;
+        this.genericName = genericName;
+        this.specificName = specificName;
+        this.recorder = recorder;
+        this.location = location;
+        this.country = country;
+        this.lat = lat;
+        this.lng = lng;
+        this.xenoCantoURL = xenoCantoURL;
+        this.soundType = soundType;
+        this.soundURL = soundURL;
+    }
+
+    public int getTagID(){
+        return this.tagID;
+    }
+
+    public void setTagId(int tagID){
+        this.tagID  = tagID;
+    }
+
+    public int getBirdID(){
+        return this.birdID;
+    }
+
+    public void setBirdId(int birdId){
+        this.birdID = birdId;
+    }
+
+    public String getEnglishName(){
+        return this.englishName;
+    }
+
+    public void setEnglishName(String englishName){
+        this.englishName = englishName;
+    }
+
+    public String getGenericName(){
+        return this.genericName;
+    }
+
+    public void setGenericName(String genericName){
+        this.genericName = genericName;
+    }
+
+    public String getSpecificName(){
+        return this.specificName;
+    }
+
+    public void setSpecificName(String specificName){
+        this.specificName = specificName;
+    }
+
+    public String getRecorder(){
+        return this.recorder;
+    }
+
+    public void setRecorder(String recorder){
+        this.recorder = recorder;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+
+    public String getCountry(){
+        return this.country;
+    }
+
+    public void setCountry(String country){
+        this.country = country;
+    }
+
+    public String getLat(){
+        return this.lat;
+    }
+
+    public void setLat(String lat){
+        this.lat = lat;
+    }
+
+    public String getLng(){
+        return this.lng;
+    }
+
+    public String getXenoCantoURL(){
+        return this.xenoCantoURL;
+    }
+
+    public void setXenoCantoURL(String xenoCantoURL){
+        this.xenoCantoURL = xenoCantoURL;
+    }
+
+    public String getSoundType(){
+        return this.soundType;
+    }
+
+    public void setSoundType(String soundType){
+        this.soundType = soundType;
+    }
+
+    public String getSoundURL(){
+        return this.soundURL;
+    }
+
+    public void setSoundURL(String soundURL){
+        this.soundURL = soundURL;
+    }
+
+}
+
