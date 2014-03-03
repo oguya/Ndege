a5e9ab4838649f0bb108dc1ecb479f4ddde7abae
oguya <oguyajames@gmail.com>
2014-03-03 17:36:41 +0300

    adding MatchResult model

---
+++ ndege/src/main/java/com/droid/ndege/model/MatchResult.java
@@ -0,0 +1,71 @@
+package com.droid.ndege.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by james on 03/03/14.
+ *
+ * A parcelable model to handle audio match results from service to activity
+ * contains:
+ *          - tagID of the new id'd tag
+ *          - tagResults of the audio matching  op.
+ *
+ */
+public class MatchResult implements Parcelable {
+
+    private int tagID;
+    private int tagResults;
+
+    public MatchResult(){}
+
+    public MatchResult(int tagID, int tagResults){
+        this.tagID = tagID;
+        this.tagResults = tagResults;
+    }
+
+    public MatchResult(Parcel inParcel){
+        this.tagID = inParcel.readInt();
+        this.tagResults = inParcel.readInt();
+    }
+
+    public int getTagID(){
+        return this.tagID;
+    }
+
+    public void setTagID(int tagID){
+        this.tagID = tagID;
+    }
+
+    public int getTagResults(){
+        return this.tagResults;
+    }
+
+    public void setTagResults(int tagResults){
+        this.tagResults = tagResults;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel outParcel, int flags) {
+        outParcel.writeInt(tagID);
+        outParcel.writeInt(tagResults);
+    }
+
+    public static final Creator<MatchResult> CREATOR = new Creator<MatchResult>() {
+        @Override
+        public MatchResult createFromParcel(Parcel source) {
+            return new MatchResult(source);
+        }
+
+        @Override
+        public MatchResult[] newArray(int size) {
+            return new MatchResult[size];
+        }
+    };
+}
